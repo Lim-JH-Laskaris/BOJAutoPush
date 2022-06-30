02bfbf41a56f93ff2cad44e457e26e7ccfe192f3
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-06-30 23:00:16 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,11 @@
+kg = int(input())
+n  = 0
+def count_bag(kg , kg_per_3):
+    kg_per_5 = kg - kg_per_3
+    if kg in [1,2,4,7]: return -1
+    if kg_per_5%5==0 and kg_per_3%3==0 : return kg_per_5//5 + kg_per_3//3
+    else : return count_bag(kg , kg_per_3+1)
+
+print(count_bag(kg, 0))
+
+    
