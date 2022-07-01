95345b427c3442ce2c2f225a2674e0e6d9b3e6ba
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-07-02 04:00:28 +0900

    [Silver V] Title: 소수, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -1,5 +1,8 @@
+def is_prime_number(number):
+    return all(number%j for j in range(2,int(number**.5 +1))) and number>1
+
 m,n = int(input()), int(input())
-prime_numbers = [i for i in range(m,n+1)if all(i%j for j in range(2,int(i**.5 +1))) and i>1]
+prime_numbers = [i for i in range(m,n+1)if is_prime_number(i)]
 if len(prime_numbers)==0: 
     print(-1)
 else : 
