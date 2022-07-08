1e4674a0868dfb2729ad1f87cfafdda8de1dc385
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-07-09 08:49:15 +0900

    [level 2] Title: 순위 검색, Time: 4014.62 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -1,7 +1,7 @@
-
 import itertools
 
 class ScoreDict(dict):
+    """총 24가지 경우의 조건에 해당하는 점수를 각각 모으고 높은 점수 순으로 정렬한 딕셔너리"""
     def __init__(self, list_info):
         self.search_space = [
             ['cpp', 'java', 'python'],  
@@ -18,14 +18,13 @@ class ScoreDict(dict):
             self['_'.join(i)].sort(reverse=True)
         
     def run_query(self, one_query):
+        """하나의 쿼리에 대해 쿼리의 결과를 출력하는 메소드"""
         search_space = [[one_query[i]] if one_query[i]!='-' else self.search_space[i] for i in range(4)]
         search_space = ['_'.join(i) for i in itertools.product(*search_space)]
-        sum_ = 0
-        for i in search_space:
-            sum_ += binary_compare_count(self[i], one_query[4])         
-        return sum_
+        return sum([binary_compare_count(self[i], one_query[4]) for i in search_space])   
 
 def binary_compare_count(reverse_sorted_list, number):
+    """이진 탐색 방식으로, 역정렬된 리스트에서 특정 숫자보다 큰 숫자의 개수를 찾는 함수"""
     a,b = 0, len(reverse_sorted_list)
     while a<b:
         mid = (a+b) // 2
@@ -47,8 +46,5 @@ def solution(info, query):
     list_info = list(map(split_and_int,info))
     list_query = list(map(split_and_int,map(replace,query))) 
     score_dict = ScoreDict(list_info)
-    
-    answer = []
-    for one_query in list_query:
-        answer.append(score_dict.run_query(one_query))
+    answer = [score_dict.run_query(one_query) for one_query in list_query]
     return answer
