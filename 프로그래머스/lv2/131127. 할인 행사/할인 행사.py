cbe09f71e9a07c65ae66d5068acd8fd69e6c7fac
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-11-08 09:30:22 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -1,15 +1,15 @@
 def int_doscount(want, discount):
-    # want의 원소를 기준으로 discount의 원소를 정수화, 없는 변수는 모두 11로
+    # want의 원소를 기준으로 discount의 원소를 정수화, 없는 변수는 모두 10로
     want_dict = {want[i]:i for i in range(len(want))}
-    return [want_dict[j] if j in want else 11 for j in discount]
+    return [want_dict[j] if j in want else 10 for j in discount]
 
 def number_to_len11(number):
     # number를 길이가 11인 리스트로 변환 (나머지는 모두 0)
-    return number+[0]*(12-len(number))
+    return number+[0]*(11-len(number))
 
 def count_by_group(list_):
     # discount의 일부분(10연속)을 가져와 number와 같은형식으로 변환
-    count_list = [0]*12
+    count_list = [0]*11
     for i in list_:
         count_list[i] += 1
     return count_list
