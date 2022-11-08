f7941184992b87a8fac10e075821d7516388f517
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-11-08 10:18:40 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -1,27 +1,26 @@
-def int_discount(want, discount):
-    # want의 원소를 기준으로 discount의 원소를 정수화, 없는 변수는 모두 10로
-    want_dict = {want[i]:i for i in range(len(want))}
-    return [want_dict[j] if j in want else 10 for j in discount]
-
-def number_to_len11(number):
-    # number를 길이가 11인 리스트로 변환 (나머지는 모두 0)
-    return number+[0]*(11-len(number))
-
-def count_by_group(list_):
-    # discount의 일부분(10연속)을 가져와 number와 같은형식으로 변환
-    count_list = [0]*11
-    for i in list_:
-        count_list[i] += 1
-    return count_list
+from collections import defaultdict
+from copy import deepcopy
 
 def solution(want, number, discount):
-    answer = 0
-    # want의 원소를 기준으로 discount의 원소를 정수화, 없는 변수는 모두 11로
-    discount = int_discount(want, discount)
-    # number를 길이가 11인 리스트로 변환 (나머지는 모두 0)
-    number = number_to_len11(number)
-    # discount의 일부분(10연속)을 가져와 number와 같은형식으로 변환하고 서로 동일한지 확인하고 카운트
-    c = 0
-    for i in range(len(discount)-9):
-        if number == count_by_group(discount[i:i+10]): c+=1
+    # discount 기준, 전체 상품 기본 dict 생성. 예) {상품:0}
+    all_product = defaultdict(int)
+    for i in list(set(discount)):
+        all_product[i]
+        
+    # 기본dict를 바탕으로 원하는 상품의 수량을 덧씌움
+    want_num_dict = deepcopy(all_product)
+    for w,n in zip(want,number):
+        want_num_dict[w] =n
+        
+    # 기본dict를 바탕으로 첫10일간의 할인 상품들을 count하고, 일치여부판단
+    discount10day_dict = deepcopy(all_product)
+    for i in discount[0:10]:
+        discount10day_dict[i] +=1
+    c = 1 if want_num_dict == discount10day_dict else 0
+    
+    # 그다음부터의 계산은 기존의 dict에서 빼고 더하는 식으로 계산량 줄임 
+    for i in range(len(discount)-10):
+        discount10day_dict[discount[i]] -= 1
+        discount10day_dict[discount[i+10]] += 1 
+        if want_num_dict == discount10day_dict: c+=1
     return c
