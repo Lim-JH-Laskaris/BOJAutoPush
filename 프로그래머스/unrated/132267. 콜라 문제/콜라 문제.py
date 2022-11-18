76f35626743c0d40a37882d77920f096d5f64e30
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-11-18 19:41:07 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10000) # 이 부분 질문하기 게시판의 답변 참고
+sys.setrecursionlimit(3000) # 이 부분 질문하기 게시판의 답변 참고
 
 def solution(a:int, b:int, n:int, c:int=0):
     """ 빈병으로 받을 수 있는 콜라 수를 반환하는 재귀함수 입니다.
@@ -8,12 +8,16 @@ def solution(a:int, b:int, n:int, c:int=0):
         b : 빈 병 a개를 가져다 주면 마트가 주는 콜라 병 수
         n : 상빈이가 가지고 있는 빈 병의 개수
         c : 현재까지 받은 콜라의 누적수량, 초기값 0으로 지정
+        
     Returns:
         a>n 인 경우, 재귀과정을 종료하고 누적수량을 반환. 
         그렇지 않은 경우, 가능한 빈병을 모두 주고 콜라를 받는 과정을 수행 한 값을 
         인수로 주고 해당 함수를 반환
+        
+    Notes:
+        처음엔 재귀한계 탓에 테스트 12번에서 런타임에러 발생.
+        재귀한계를 통과 가능한 수준으로 수정
     """
-
     if a>n : return c
     else   : return solution(a,b,b*(n//a)+n%a,c+b*(n//a))
     
