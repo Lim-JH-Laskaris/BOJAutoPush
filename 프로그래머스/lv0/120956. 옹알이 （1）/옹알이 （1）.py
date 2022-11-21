907ca6b987b48cd1a570d34899745a24304cfbc6
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-11-21 20:19:57 +0900

    [level 0] Title: 옹알이 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,17 +1,10 @@
-def is_ongalyi(str_):
-    ongalyi2 = ["ye", "ma"]
-    ongalyi3 = ["aya", "woo"]
-    if   len(str_) == 0 : 
-        return 1
-    elif len(str_) == 1 : 
-        return 0
-    elif str_[:2] in ongalyi2 : 
-        return is_ongalyi(str_[2:])
-    elif len(str_)>=3 and str_[:3] in ongalyi3 : 
-        return is_ongalyi(str_[3:])
-    else : 
-        return 0 
+def is_babbling(str_):
+    pronunciation = ["aya", "ye", "woo", "ma"]
+    for i in pronunciation:
+        str_ = str_.replace(i,' ')
+    return 1 if str_.isspace() else 0 
+    
 
 def solution(babbling):
-    ongalyi_list = [is_ongalyi(i) for i in babbling]
-    return sum(ongalyi_list)
+    babbling_list = [is_babbling(i) for i in babbling]
+    return sum(babbling_list)
