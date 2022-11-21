e392d60d4765fa571e5de6b62158c076925ad9a4
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-11-21 21:00:01 +0900

    [unrated] Title: 옹알이 (2), Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,4 +1,13 @@
 def is_babbling(word):
+    """한 단어가 조카가 발음가능한지 여부를 0,1로 반환
+    
+    Args :
+        word (str) : 발음가능한지 판단해야하는 단어
+    
+    Returns :
+        같은 발음이 연속되거나 불가능한 발음이 있으면 0
+        그렇지 않은 경우 1
+    """
     pronunciation = ["aya", "ye", "woo", "ma"]
     for i in range(len(pronunciation)):
         word = word.replace(pronunciation[i],f'{i}')
