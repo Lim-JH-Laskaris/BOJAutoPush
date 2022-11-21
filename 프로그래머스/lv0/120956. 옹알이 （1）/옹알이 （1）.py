9a2236fe08c2b10c5792ddf3d35c73488e3ae274
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-11-21 20:26:25 +0900

    [level 0] Title: 옹알이 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,10 +1,9 @@
-def is_babbling(str_):
+def is_babbling(word):
     pronunciation = ["aya", "ye", "woo", "ma"]
     for i in pronunciation:
-        str_ = str_.replace(i,' ')
-    return 1 if str_.isspace() else 0 
+        word = word.replace(i,' ')
+    return 1 if word.isspace() else 0 
     
-
 def solution(babbling):
     babbling_list = [is_babbling(i) for i in babbling]
     return sum(babbling_list)
