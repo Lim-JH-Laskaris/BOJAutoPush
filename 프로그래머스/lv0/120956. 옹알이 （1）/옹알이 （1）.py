b4b4fe38e8170c92378343f0f2669cf8a5841a03
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-11-21 21:52:27 +0900

    [level 0] Title: 옹알이 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,12 +1,10 @@
 def is_babbling(word):
     pronunciation = ["aya", "ye", "woo", "ma"]
     for i in pronunciation:
-        word = word.replace(i,' ',1)
+        word = word.replace(i,' ',1) 
+        # 제한사항에 한번만 등장한다 되어있으니 세번째 인수 1 없어도 무관
     return 1 if word.isspace() else 0 
     
 def solution(babbling):
     babbling_list = [is_babbling(i) for i in babbling]
     return sum(babbling_list)
-
-# 문제에는 최대 한번씩만 사용할 수 있다고 하는데 그걸 고려하지 않아도 통과되네요
-# word = word.replace(i,' ',1)을 word = word.replace(i,' ')로만 써도 되는 이상이 있습니다.
