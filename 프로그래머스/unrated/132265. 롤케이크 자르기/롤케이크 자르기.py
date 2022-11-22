1cf51322d17a4b48790845bac4205f08718ca989
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-11-22 18:36:53 +0900

    [unrated] Title: 롤케이크 자르기, Time: 132.96 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,23 +1,17 @@
 from collections import Counter
-from collections import deque
 
 def solution(topping):
     count_topping = dict(Counter(topping))
-    deque_topping = deque(topping)
     l_side = set()
+    l_side_c = 0
     r_side_c = len(set(topping))
     equal_cut = 0
-    for i in range(len(topping)):
-        i_popleft = deque_topping.popleft()
-        l_side.add(i_popleft)
-        l_side_c = len(l_side)
-        count_topping[i_popleft] -= 1
-        r_side_c = r_side_c-1 if count_topping[i_popleft] == 0 else r_side_c
-        if   l_side_c == r_side_c : equal_cut += 1
-        elif l_side_c >  r_side_c : break
+    for i in topping:
+        count_topping[i] -= 1
+        if i not in l_side : 
+            l_side.add(i)
+            l_side_c = len(l_side)
+        if   count_topping[i] == 0 : r_side_c -=1
+        if   l_side_c == r_side_c  : equal_cut += 1
+        elif l_side_c >  r_side_c  : break
     return equal_cut
-
-
-#시도1 - 시간초과 : 중복되는 부분을 제거 필요
-# def solution(topping):
-#     return sum([1 for i in range(1,len(topping)) if len(set(topping[:i]))==len(set(topping[i:]))])
