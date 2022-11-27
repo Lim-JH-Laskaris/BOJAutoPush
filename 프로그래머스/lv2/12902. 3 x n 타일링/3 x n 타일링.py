182105c4b2b24242abb84186a75ae8429fb0ffd8
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-11-28 08:36:35 +0900

    [level 2] Title: 3 x n 타일링, Time: 1.25 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.py
+++ 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -7,8 +7,26 @@ class TilingDict(dict):
         self[k] = (self[k-2]*4-self[k-4])%1000000007
         return self[k]
 
-
 def solution(n):
     td = TilingDict()
     for i in range(n//200): td[i*200]
     return td[n]
+
+"""
+점화식이 
+s[k] = s[k-2]*4-s[k-4]
+의 형태를 가지는 이유 해설.
+
+https://s2choco.tistory.com/24
+이곳 등 대부분의 글에서는 
+s[k] = s[k-2]*3+s[k-4]*2+s[k-6]*2+...+s[4]*2++s[2]*2+2
+의 점화식으로 문제를 풀고있다. 
+
+그런데 위의 식에서 
+s[k-2] = s[k-4]*3+s[k-6]*2+...+s[4]*2++s[2]*2+2
+이고 양변에 s[k-4]를 빼면
+s[k-2]-s[k-2] = s[k-4]*2+s[k-6]*2+...+s[4]*2++s[2]*2+2
+인데, 이는 위의 s[k]에 대한 점화식의 s[k-2]*3+~~ 이후부분과 동일하므로
+s[k] = s[k-2]*3+s[k-2]-s[k-2] = s[k-2]*4-s[k-2]
+이렇게 단순화 할 수 있다.
+"""
