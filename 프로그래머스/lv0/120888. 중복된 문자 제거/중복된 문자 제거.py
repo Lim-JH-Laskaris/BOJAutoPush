544b9e7a8191d8d7cd73338b30be062f1244023a
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-12-14 09:12:01 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,8 +1,1 @@
-solution = lambda my_string, answer='': answer if my_string=='' else (
-    solution(my_string[1:],answer) if my_string[0] in answer
-    else solution(my_string[1:],answer+my_string[0])
-)
-
-"""출력할 문자열을 answer에 하나씩 쌓아갑니다. my_string문자를 맨앞부터 하나씩 검사하여, 
-answer에 있으면 넘어가고, 없으면 추가합니다. my_string의 끝에 도달하면 answer를 출력합니다.
-"""
+solution = lambda my_string : ''.join(dict.fromkeys(my_string))
