3a14c55e2b1e7d387a688c2745ac595856626bb0
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-12-16 23:39:22 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,5 +1,4 @@
 solution = lambda numbers, count=0: solution(
-    numbers.replace(
-        ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"][count], 
-        str(count)), 
+    numbers.replace(["zero", "one", "two", "three", "four", "five", 
+                     "six", "seven", "eight", "nine"][count], str(count)), 
     count+1) if count<10 else int(numbers)
