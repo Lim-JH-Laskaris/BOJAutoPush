9c6214590d62e4bcbf32c4373cbf3cf8dc82a2d4
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-12-23 22:55:29 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -1,1 +1,1 @@
-solution = lambda my_string : my_string.replace('a','').replace('e','').replace('i','').replace('o','').replace('u','')
+solution = lambda my_string, c=0 : solution(my_string.replace('aeiou'[c],''), c+1) if c<5 else my_string
