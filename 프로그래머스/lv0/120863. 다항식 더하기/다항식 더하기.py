fa3cc7fb4f8cc3aae1855b42d58d1a3ee7de2c2a
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2022-12-30 22:52:14 +0900

    [level 0] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
@@ -2,24 +2,23 @@ def solution(polynomial):
     polynomial = ' '+polynomial+' '
     polynomial = polynomial.replace(' x ',' 1x ')[1:-1]
     poly_list = polynomial.split(' + ')
+    
     answer = [0,0]
     while poly_list :
         if poly_list[-1][-1] =='x':
             answer[0] += int(poly_list.pop()[:-1])
         else :
             answer[1] += int(poly_list.pop())
-    if answer[0] == 0 :
-        if answer[1] == 0 : 
-            return ''
-        else :
-            return str(answer[1])
-    elif answer[0] == 1:
-        if answer[1] == 0 : 
-            return 'x'
-        else :
-            return f'x + {answer[1]}'
-    else:
-        if answer[1] == 0 : 
-            return str(answer[0])+'x'
-        else :
-            return f'{answer[0]}x + {answer[1]}'
+            
+    if   answer[0] == 0 : answer_str = ''
+    elif answer[0] == 1 : answer_str = 'x'
+    else : answer_str = f'{answer[0]}x'
+         
+    if answer[1] > 0 and answer_str != '' : 
+        answer_str = answer_str+' + '
+            
+    if answer[1] > 0 : 
+        answer_str = answer_str+f'{answer[1]}'
+        
+    return answer_str
+    
