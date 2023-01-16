61da4527d8335f080030082484b6ea855cc16fa0
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-01-16 18:28:45 +0900

    [level 2] Title: 올바른 괄호, Time: 6.71 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,15 +1,14 @@
-#import sys 
-#sys.setrecursionlimit(10**5)
-
-#solution = lambda s ,stack=0:(True if stack==0 else False) if len(s)==0 else (
-#    False if stack<0 else solution(s[1:], stack+1 if s[0]=='(' else stack-1))
-## 효율성 테스트 통과 못함
-
 def solution(s):
+    """ s 문자열의 마지막 문자부터 하나씩 뽑는다. (list의 pop 효율성을 위해)
+    ) 문자면 stack 변수를 +1, (문자면 -1을 한다. 
+    이때 중간에라도 stack 변수가 음수가 된다면 이는 )(() 와 같은 경우로,
+    괄호가 비정상적인 경우이다. 
+    s 문자열을 모두 뽑았다면 stack 변수가 0인지 아닌지에 따라 
+    
+    """
     stack = 0
-    s=list(s)
-    while s:
-        a = s.pop()
-        stack = stack+1 if a==')' else stack-1
+    for i in s:
+        stack = stack+1 if i=='(' else stack-1
         if stack < 0 : return False 
     return True if stack==0 else False
+
