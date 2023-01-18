42a37e9cbb44e0698f687181fb12a0bc07fef486
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-01-18 13:33:08 +0900

    [level 2] Title: 멀리 뛰기, Time: 1.13 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -7,7 +7,9 @@ class FibDict(dict):
         self[k] = (self[k-1]+self[k-2])%1234567
         return self[k]
 
-def solution(n):
-    fib_dict = FibDict()
-    return [fib_dict[i] for i in range(n%200,n+1,200)][-1]
+fib_dict = FibDict()  
+    #클래스 인스턴스 자체는 다음 문제들에 재활용될 수 있다.
+    
+solution = lambda n : [fib_dict[i] for i in range(n%200,n+1,200)][-1]
     #재귀한계문제를 해결하기위해 적당한 단위로 끊어서 호출해 메모한다.
+    
