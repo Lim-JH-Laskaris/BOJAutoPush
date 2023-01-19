20349f28cbc14b30e3fd907914f469182ed69eba
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-01-19 15:01:34 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -5,3 +5,5 @@ def solution(ingredient):
         if len(stack)>=4 and stack[-4:] == '1231':
             stack = stack[:-4] 
     return (len(ingredient)-len(stack))//4
+
+#ingredient와 stack을 문자열이 아닌 리스트로 사용할 경우 시간초과가 된다. 
