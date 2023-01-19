7a1a845ef2ab32ce8f6a864e29b56d01c1e710e2
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-01-19 15:03:17 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -6,4 +6,5 @@ def solution(ingredient):
             stack = stack[:-4] 
     return (len(ingredient)-len(stack))//4
 
-#ingredient와 stack을 문자열이 아닌 리스트로 사용할 경우 시간초과가 된다. 
+# ingredient와 stack을 문자열이 아닌 리스트로 사용할 경우 시간초과가 된다. 
+# 한계상황에서 리스트에 비해 문자열이 10~20배 정도 빠른듯하다.
