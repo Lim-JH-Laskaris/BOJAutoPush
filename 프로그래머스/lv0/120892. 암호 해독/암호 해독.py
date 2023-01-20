4c20abc1a10d63a2a5b7aa3400226ef5c1de4359
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-01-21 01:18:09 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,1 +1,2 @@
-solution = lambda cipher, code : ''.join([cipher[i] for i in range(code-1,len(cipher),code)])
+#solution = lambda cipher, code : ''.join([cipher[i] for i in range(code-1,len(cipher),code)])
+solution = lambda cipher, code : cipher[code-1::code]
