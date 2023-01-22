81bde6c0ca7cafbc1ebc422db8b6d1ac0ce23780
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-01-22 15:44:05 +0900

    [unrated] Title: 문자열 나누기, Time: 12.08 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
@@ -1,11 +1,11 @@
 import sys
 sys.setrecursionlimit(10000)
 
-def solution(s,n=0):
+def solution(s,n=1):
     x0,x1 = 0,0
     x = s[0]
     for i in range(len(s)):
         if x == s[i] : x1 += 1
         else : x0 += 1
         if x0 == x1 and i<len(s)-1: return solution(s[i+1:],n+1)
-    return n+1
+    return n
