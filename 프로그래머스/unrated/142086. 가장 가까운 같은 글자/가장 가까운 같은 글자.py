d7bee9197420a056766f2593f044aadfe8a6996f
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-01-30 13:34:53 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 8.56 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,12 +1,11 @@
 from collections import deque
 
 def solution(s):
-    pre = deque()
-    pro = deque(s)
+    pre, pro = deque(), deque(s)
     result = []
     while pro :
         pop = pro.popleft()
-        if not pop in pre : result.append(-1)
+        if pop not in pre : result.append(-1)
         else : result.append(pre.index(pop)+1)
         pre.appendleft(pop)
     return result
