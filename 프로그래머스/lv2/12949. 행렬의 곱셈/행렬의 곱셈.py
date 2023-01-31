63b2e80bcdd162aa10081e0e01e62fa36b517034
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-01-31 16:15:13 +0900

    [level 2] Title: 행렬의 곱셈, Time: 0.30 ms, Memory: 28.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,4 +1,2 @@
 import numpy as np
-
-solution = lambda arr1, arr2 : list(map(lambda x :list(map(int,x)),np.array(arr1)@np.array(arr2)))
-    
+solution = lambda arr1, arr2 : (np.array(arr1)@np.array(arr2)).tolist()
