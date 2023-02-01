fed0620bb4c4b87916322816e99a4d6d69f4c80c
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-02-01 09:34:28 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.64 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -3,5 +3,5 @@ def solution(number, limit, power):
     for i in range(1,number+1):
         for j in range(i,number+1,i):
             divisor_count_list[j] += 1
-    weapon_list = list(map(lambda x:x if x<=limit else power, divisor_count_list)) 
-    return sum(weapon_list)
+    weapon_map = map(lambda x:x if x<=limit else power, divisor_count_list)
+    return sum(weapon_map)
