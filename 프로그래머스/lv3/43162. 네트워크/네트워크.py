34d524ddb74ff29109b5511f57f0afd6782895f8
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-02-28 01:40:23 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solution(n, computers):
+    answer = 0
+    check_list = [0]*n   
+    link_dict = {i:[j for j in range(n) if computers[i][j]==1] for i in range(n)}
+    for i in range(n):
+        if check_list[i] == 0:
+            queue = deque([i])
+            while queue:
+                p = queue.popleft()
+                if check_list[p] == 0:
+                    check_list[p] = 1 
+                    queue.extend(link_dict[p])
+            answer += 1
+    return answer
