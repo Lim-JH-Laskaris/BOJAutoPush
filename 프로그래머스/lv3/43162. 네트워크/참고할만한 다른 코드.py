c2c0ab274766940f65f3f11473b52f1943b3ff40
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-02-28 01:49:38 +0900

    Create 참고할만한 다른 코드.py

---
+++ 프로그래머스/lv3/43162. 네트워크/참고할만한 다른 코드.py
@@ -0,0 +1,12 @@
+#플로이드-워셜 알고리즘 이용
+def solution(n, computers):
+    temp = []
+    for i in range(n):
+        temp.append(i)
+    for i in range(n):
+        for j in range(n):
+            if computers[i][j]:
+                for k in range(n):
+                    if temp[k] == temp[i]:
+                        temp[k] = temp[j]
+    return len(set(temp))
