87a4f0c3e42cfc21e98d73d5065393eaaf010ce6
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-03-02 17:58:19 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -2,6 +2,19 @@ def solution(k, score):
     honor_list = []
     result = []
     for i in score:
+        # honor_list = insert(honor_list, i)[:k] 
         honor_list = sorted(honor_list+[i],reverse=True)[:k]
         result.append(honor_list[-1])
     return result
+
+#매번 sort하는 것은 k에대해 선형로그만큼 커지므로 그때그때 삽입하는 방식으로 코드를 짰으나, 오히려 시간이 더 걸리게 되었다. 
+# def insert(list_, n):
+#     idx = 0
+#     for i in range(len(list_)):
+#         if list_[i]>n:
+#             idx += 1
+#         else :
+#             break
+#     list_.insert(idx,n)
+#     return list_
+
