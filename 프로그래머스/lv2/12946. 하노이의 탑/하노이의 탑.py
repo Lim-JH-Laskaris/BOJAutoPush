7be9b29c1aeafcd0fe69ed6469ff09bb05b5d38d
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-03-19 10:15:25 +0900

    [level 2] Title: 하노이의 탑, Time: 17.45 ms, Memory: 18.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,4 +1,4 @@
-def transAtoC(result, a, c):
+def from13toAC(result, a, c):
     """1번 기둥에서 3번기둥으로 원판들을 옮기는 답안을, a에서 c로 옮기는 답으로 변환.
     즉, 답안의 1을 a, 2를 b, 3을 c로 변환하는 함수.
     """
@@ -8,13 +8,14 @@ def transAtoC(result, a, c):
     
 def solution(n):
     result = []
-    for i in range(1,n+1):
-        result = transAtoC(result,1,2)+[[1,3]]+transAtoC(result,2,3)
+    for i in range(n):
+        result = from13toAC(result,1,2)+[[1,3]]+from13toAC(result,2,3)
     return result
 
 """
 n-1개의 원판을 1에서 3으로 옮기는 과정(이하 n-1의 1-3과정)을 기록했다고 하자.
 그렇다면 n개의 원판을 1에서 3으로 옮기는 과정(이하 n의 1-3과정)은 다음같이 나타낼 수 있다.
+
 1. n-1의 1-2과정
 2. 맨 아래 원판을 1에서 3으로 옮김
 3. n-1의 2-3과정
