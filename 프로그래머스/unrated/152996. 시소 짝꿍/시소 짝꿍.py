b4ef22e12b11afd761b14b76ebd7183111fafbd4
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-04-24 07:08:11 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -10,3 +10,7 @@ def solution(weights):
         if k%2==0 and k*3//2 in keys : answer += count[k]*count[k*3//2]
         if k%3==0 and k*4//3 in keys : answer += count[k]*count[k*4//3]
     return answer
+
+"""인원수는 최대 10**6으로 많지만, 값의 범위는 900에 정수 한정이라 Counter를 이용해 
+푸는 것으로 계산 중복을 크게 줄일수 있었다. 정수 조건이 없거나 값의 범위가 인원수 보다 많았다면 
+이 방법으로 복잡도를 유의미하게 줄이지는 못했을 것"""
