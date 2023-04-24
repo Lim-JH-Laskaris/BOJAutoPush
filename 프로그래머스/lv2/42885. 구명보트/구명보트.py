3b51031a1a0e31829e035acfc3b7981cdceaf9dc
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-04-25 02:23:49 +0900

    [level 2] Title: 구명보트, Time: 8.67 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,3 +1,14 @@
+from collections import deque
+def solution(people, limit):
+    people = deque(sorted(people))
+    answer = 0
+    while people:
+        a = people.pop()
+        if people and people[0] <= limit-a:
+            people.popleft()
+        answer += 1
+    return answer
+
 # 최초 코드, 효율성 문제
 # def solution(people, limit):
 #     """탐욕법: 가장 무거운 사람부터 태우고, 동승 가능 인원중 가장 무거운 이부터 동승시킨다."""
@@ -11,6 +22,7 @@
 #         answer += 1
 #     return answer
 
+# Counter를 통해 개선했으나 여전히 효율성 문제.
 # from collections import Counter
 # def solution(people, limit):
 #     people = Counter(people)
@@ -26,14 +38,5 @@
 #             else : people[b] -= 1
 #         answer += 1
 #     return answer
-
-from collections import deque
-def solution(people, limit):
-    people = deque(sorted(people))
-    answer = 0
-    while people:
-        a = people.pop()
-        if people and people[0] <= limit-a:
-            people.popleft()
-        answer += 1
-    return answer
+"""인원수는 2명으로 제한되어 있을때는 굳이 동승자 b의 무게가
+최대가 되게 할 필요가 없음을 알고 이를 고쳐서 해결"""
