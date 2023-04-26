972272256891492be89c76e46412a18785b36896
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-04-27 08:43:31 +0900

    Create 참고할만한 다른 코드.py

---
+++ 프로그래머스/lv3/42861. 섬 연결하기/참고할만한 다른 코드.py
@@ -0,0 +1,23 @@
+# 크루스칼 알고리즘을 이용한 풀
+#조원형 2023.2.2 11:40
+def solution(n, costs):
+    answer = 0
+    uni = [0] * (n + 1)
+    costs.sort(key = lambda x: x[2])
+    for i in range(1,n + 1):
+        uni[i] = i
+    def union(x,y):
+        a = find(x)
+        b = find(y)
+        if a != b:
+            uni[a] = b
+    def find(x):
+        if uni[x] == x: return x
+        else:
+            uni[x] = find(uni[x])
+            return uni[x]
+    for n1,n2,cost in costs:
+        if find(n1) != find(n2):
+            union(n1,n2)
+            answer += cost
+    return answer
