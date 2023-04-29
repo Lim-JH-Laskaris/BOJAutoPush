a59aca7b7227a900eeaf969fd53c02100e9224f4
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-04-29 10:58:50 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 653.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -3,11 +3,10 @@
 def solution(r1, r2):
     answer = 0
     # 우선 하나의 사분면에서만 계산 
-    for i in range(1,r2): # r2포함 점 셈. 단 x축 y축 상의 점 빼고
+    for i in range(0,r2): # r2포함 점 셈. 단 y축 상의 점 빼고
         answer += int((r2**2-i**2)**.5)
-    for i in range(1,r1): # r1포함 점 뺌. 단 x축 y축 상의 점 빼고
+    for i in range(0,r1): # r1포함 점 뺌. 단 y축 상의 점 빼고
         answer -= int((r1**2-i**2)**.5)
         if int((r1**2-i**2)**.5) == (r1**2-i**2)**.5 : 
             answer += 1 # r1의 경계에 있는 점 다시 추가
-    answer += r2-r1+1 # 축 상의 점 추가 
     return answer*4 # 전체 좌표평면으로 확대
