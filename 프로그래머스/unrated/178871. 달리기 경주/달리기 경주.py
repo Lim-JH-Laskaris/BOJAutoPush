ab11b573c8712a8193c30645ce3cbaba578ee09c
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-04-29 11:25:17 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -7,3 +7,10 @@ def solution(players, callings):
         pi[u],pi[d],ip[i],ip[i-1] = i-1,i,d,u
     players = [ip[i] for i in range(len(players))]
     return players
+
+# 처음엔 아래와 같이 리스트에서 직접 바꿔주는 방법을 취했는데, 시간 초과가 떴다.
+# def solution(players, callings):
+#     for c in callings:
+#         i = players.index(c)
+#         players[i-1],players[i] = players[i],players[i-1]
+#     return players
