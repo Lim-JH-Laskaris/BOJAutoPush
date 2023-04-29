df4c6a6ef7e68b8fff5b0ea5d04b9e85f09f6f6e
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-04-29 14:59:17 +0900

    [unrated] Title: 혼자서 하는 틱택토, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
@@ -1,17 +1,14 @@
 def solution(board):
-    t_board = [board[0][i]+board[1][i]+board[2][i] for i in range(3)]
-    x_board = [board[0][0]+board[1][1]+board[2][2],
-               board[0][2]+board[1][1]+board[2][0]]
-    l_board = list(board[0]+board[1]+board[2])
+    t_board = [board[0][i]+board[1][i]+board[2][i] for i in range(3)] #전치행렬
+    x_board = [board[0][0]+board[1][1]+board[2][2], 
+               board[0][2]+board[1][1]+board[2][0]] # 대각선 방향에 'OOO'나'XXX'유무 확인
+    all_board = board+t_board+x_board 
+    l_board = list(board[0]+board[1]+board[2]) # 아래에서 o,x 개수 세기 위함
     o = l_board.count('O')
     x = l_board.count('X')
-    if x>o or o>x+1: return 0
-    if 'OOO' in board:
-        if 'XXX' in board or x==o: return 0
-    if 'OOO' in t_board:
-        if 'XXX' in t_board or x==o: return 0    
-    if 'OOO' in x_board:
-        if x==o: return 0 
+    if x>o or o>x+1: return 0 # 승패 상관없이, 가능한 o,x 개수인지 확인
+    if 'OOO' in all_board:
+        if 'XXX' in all_board or x==o: return 0
     if 'XXX' in board+t_board+x_board:
         if not x==o: return 0
     return 1
