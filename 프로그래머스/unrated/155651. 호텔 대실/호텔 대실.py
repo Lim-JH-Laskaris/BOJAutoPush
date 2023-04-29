45328e33fd852aff609d168ee1445e848e90fdc8
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-04-30 02:45:28 +0900

    [unrated] Title: 호텔 대실, Time: 2.18 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.py
@@ -4,15 +4,15 @@ def solution(book_time):
                          list(map(lambda y: int(y[:2])*60+int(y[3:]),x)),
                          book_time))
                 # 시각을 분단위 정수시각으로 변환
-    # book_time.sort()
-    heapify(book_time)
+    book_time.sort()
     use_heapq = [] # 종료시각만 원소로 넣음. 넣을 때 준비시간도 포함예정.
     answer = 0 # use_heapq의 최대 길이.
-    # for b in book_time:
-    while book_time:
-        while use_heapq and use_heapq[0]<=book_time[0][0] : #b시작시각 전에 끝나는 예약이 있다면,
+    for b in book_time:
+        while use_heapq and use_heapq[0]<=b[0] : #b시작시각 전에 끝나는 예약이 있다면,
             heappop(use_heapq)
-        # heappush(use_heapq,b[1]+10) #앞서 말했듯 종료시각 +10분 추가
-        heappush(use_heapq,heappop(book_time)[1]+10) #앞서 말했듯 종료시각 +10분 추가
+        heappush(use_heapq,b[1]+10) #앞서 말했듯 종료시각 +10분 추가
         answer = max(answer, len(use_heapq)) 
     return answer
+
+"""book_time.sort()가 아닌 heapify(book_time)을 한뒤 pop해서 사용하는 방식을 실험해 봤으나
+그 방법으로 할 때가 오히려 2배 정도 느린 것으로 나타난다."""
