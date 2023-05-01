514d4640a13023160ac656eb67acf9eace077172
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-05-02 08:46:03 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,5 +1,4 @@
-from functools import reduce
 def solution(A,B):
     A.sort()
     B.sort(reverse=True)
-    return reduce(lambda x,y:x+y,[x*y for x,y in zip(A,B)])
+    return sum([x*y for x,y in zip(A,B)])
