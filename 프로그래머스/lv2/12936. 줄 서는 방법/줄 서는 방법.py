4f770fa1577a2292800cd80431f492dbb7668e5b
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-05-04 08:38:09 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,11 +1,10 @@
-def factorial(x):
-    return 1 if x==0 else x*factorial(x-1)
+from math import factorial
 
 def solution(n, k):
     answer = []
-    k -= 1 
-    n = list(range(1,n+1))
+    k -= 1 # 0부터 시작하는 서수로 변환
+    n = list(range(1,n+1)) 
     while n : 
-        d, k = k//factorial(len(n)-1),k%factorial(len(n)-1)
+        d, k = divmod(k,factorial(len(n)-1))
         answer.append(n.pop(d))
     return answer
