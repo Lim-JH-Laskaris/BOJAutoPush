c5467d222dcf401f0906dab9daa2b1fd4ba9e374
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-05-04 12:04:37 +0900

    [level 2] Title: 짝지어 제거하기, Time: 111.39 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,12 +1,10 @@
 def solution(s):
     stack = []
-    s_rev = list(reversed(list(s)))
-    while s_rev: 
+    for i in s:
         if not stack : # stack이 비어있으면 채워줌
-            stack.append(s_rev.pop())
-        elif stack[-1] == s_rev[-1] : # 같으면 둘다 제거
-            s_rev.pop()
+            stack.append(i)
+        elif stack[-1] == i : # 같으면 제거
             stack.pop()
         else : # 같지 않으면 스택으로 이동
-            stack.append(s_rev.pop())  
+            stack.append(i)  
     return 0 if stack else 1 # 스택이 비어있으면 성공이므로 1
