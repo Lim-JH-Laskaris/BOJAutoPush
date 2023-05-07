d1a96046ca385a6d9d5c03375108e9883069cda7
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-05-07 10:47:40 +0900

    [unrated] Title: 귤 고르기, Time: 15.70 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -8,14 +8,3 @@ def solution(k, tangerine):
         k -= a
         answer += 1
         if k<=0 : return answer
-    
-
-def solution(k, tangerine):
-    c = Counter(tangerine) #{크기:개수}
-    cc = sorted(Counter(c.values()).items(), reverse=True) #[(개수,개수의 개수)]
-    answer = 0
-    for a,b in cc:
-        for i in range(b):
-            k -= a
-            answer += 1
-            if k<=0 : return answer
