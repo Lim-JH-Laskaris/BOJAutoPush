973d86ef5f8b2338e7a862cb25aef9b054e0784f
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-05-16 22:25:09 +0900

    [level 3] Title: 최고의 집합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py
@@ -1,9 +1,5 @@
 def solution(n, s):
     """중복집합내 원소가 균일할 수록 곱의 값이 커진다."""
     if n>s :return [-1]
-    answer = []
-    for i in range(n,0,-1):
-        j = s//i
-        answer.append(j)
-        s -= j
-    return sorted(answer)
+    d,m = divmod(s,n)
+    return [d]*(n-m)+[d+1]*m
