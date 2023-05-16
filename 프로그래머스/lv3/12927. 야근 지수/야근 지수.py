9059b697f541c6876426557a0f41f7096bd186b2
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-05-16 20:52:39 +0900

    [level 3] Title: 야근 지수, Time: 484.49 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.py
@@ -1,6 +1,7 @@
 from heapq import heappop, heappush, heapify
 
 def solution(n, works):
+    """최초코드"""
     works = list(map(lambda x:-x,works))
     heapify(works)
     while n:
@@ -11,3 +12,11 @@ def solution(n, works):
         else: 
             break
     return sum(list(map(lambda x:x**2,works)))
+
+def solution(n, works):
+    """라인 수를 줄인 개선 코드"""
+    heapify(works := [-w for w in works])
+    for i in range(n):
+        heappush(works, min(heappop(works)+1,0))
+    return sum([w**2 for w in works])
+""" := 대입과 할당을 동시에."""
