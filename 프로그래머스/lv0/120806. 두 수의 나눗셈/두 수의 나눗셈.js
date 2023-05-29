5caea3259cfc7a7f1e95f7aef022cc115f394bf7
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-05-30 00:27:29 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -1,1 +1,7 @@
-const solution = (num1, num2) => (num1*1000-(num1*1000 % num2)) / num2
+const solution = (num1, num2) => Math.floor(num1 / num2 * 1000)
+
+// const solution = (num1, num2) => Math.trunc(num1 / num2 * 1000) // 이것도 가능
+
+//Math.trunc()는 소수점 이하는 다 버린다. ex) Math.trunc(23.3) = 23, Math.trunc(-23.3) = -23
+//Math.floor()는 소수점을 내림한다. ex) Math.floor(23.3) = 23, Math.floor(-23.3) = -24가 된다. 
+//음수인 경우도 생각해서 소수점을 이하 수를 없애고 싶을때는 Math.trunc를 사용하는 것이 좋다.
