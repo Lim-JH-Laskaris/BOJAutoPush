bc18fb30084ac0f55fa91789681d4579fc3fe0c3
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-06-01 01:07:44 +0900

    [unrated] Title: 배열 비교하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
@@ -1,7 +1,8 @@
 function solution(arr1, arr2) {
+    const sum = (a,b) => a+b;
     const arrSub = arr1.length - arr2.length;
+    const sumSub = arr1.reduce(sum) - arr2.reduce(sum);
     if (arrSub != 0) {return arrSub/Math.abs(arrSub)}
-    const sumSub = arr1.reduce((a, b) => a + b, 0) - arr2.reduce((a, b) => a + b, 0);
     if (sumSub != 0) {return sumSub/Math.abs(sumSub)}
     return 0
 }
