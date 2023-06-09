f712eb433199d6d87c8b4faf164ef00416aec487
Lim-JH-Laskaris <97493872+Lim-JH-Laskaris@users.noreply.github.com>
2023-06-09 18:20:40 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,5 +1,11 @@
+// 처음 작성한 코드
 function solution(numbers) {
     var answer = [];
-    numbers.forEach(function(i){answer.push(i*2)});
+    numbers.forEach(function (i) {answer.push(i*2)});
     return answer
 }
+
+// 아래는 다른 사람의 풀이 참고
+var solution = (numbers) => numbers.reduce((a, b) => [...a, b * 2], [])
+var solution = (numbers) => numbers.map((number) => number * 2)
+
